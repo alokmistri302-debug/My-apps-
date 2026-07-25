d43eca3b78f853b04485dc8b0546afda246dc270
alokmistri302-debug <alokmistri302@gmail.com>
2026-07-25 20:56:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class DukanApp(App):
+    def build(self):
+        return Label(text='Dukan Hisaab App - Working!')
+
+if __name__ == '__main__':
+    DukanApp().run()
+  [a
